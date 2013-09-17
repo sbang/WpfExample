3514970bab57c9efb7c12940f7e5310e080e4245
Steinar Bang <steb@steria.no>
2013-09-17 21:02:47 +0200

    Created a simple WPF Control with a basic unit test.
    Used NuGet to install NUnit and the NUnit test harness.

---
+++ CustomControls-tests/HelloControlTest.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HelloControlTest.cs" company="Steria AS">
+//   Copyright Steria 2013
+// </copyright>
+// <summary>
+//   Defines the HelloControlTest type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CustomControls
+{
+    using NUnit.Framework;
+
+    [TestFixture(Description = "Tests for the HelloControl class.")]
+    public class HelloControlTest
+    {
+        [Test(Description = "Verify that creating the control doesn't throw an exception.")]
+        public void TestCreateControl()
+        {
+            var runner = new CrossThreadTestRunner();
+            runner.RunInSTA(
+              delegate
+              {
+                  var control = new HelloControl();
+                  Assert.AreEqual("TextBox", control.DisplayHelloTextBox.Text);
+              });
+        }
+    }
+}
---
+++ CustomControls/HelloControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HelloControl.xaml.cs" company="Steria AS">
+//   Copyright Steria 2013
+// </copyright>
+// <summary>
+//   Interaction logic for HelloControl.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CustomControls
+{
+    /// <summary>
+    /// Interaction logic for HelloControl.xaml
+    /// </summary>
+    public partial class HelloControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HelloControl"/> class.
+        /// </summary>
+        public HelloControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
